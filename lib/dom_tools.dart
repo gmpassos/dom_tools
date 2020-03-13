101ecaf0b949802756e94b0a3f9df194f648c922
gmpassos <gracilianomp@gmail.com>
2020-03-13 01:14:44 -0300

    move Track classes from dom_tools_base.dart to dom_tools_track.dart

--- lib/dom_tools.dart
+++ lib/dom_tools.dart
@@ -4,6 +4,7 @@ library dom_tools;
 export 'src/dom_tools_base.dart';
 export 'src/dom_tools_css.dart';
 export 'src/dom_tools_document.dart';
+export 'src/dom_tools_track.dart';
 export 'src/dom_tools_paint.dart';
 export 'src/perspective_filter.dart';
 export 'src/dom_data_storage.dart';
